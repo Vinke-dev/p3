10bee950fd78943602f10e055c7c80e211047849
Thierry Chappuis <tchappui@users.noreply.github.com>
2019-11-14 07:44:34 +0100

    Update position.py

--- position.py
+++ position.py
@@ -17,4 +17,7 @@ class Position:
     def right(self):
         return Position(self.x + 1, self.y)
 
-    
+    # -tc- Utile également d'ajouter une méthode __eq__(self, other) pour
+    # -tc- comparer deux positions entre elles et __hash__(self) pour 
+    # -tc- pouvoir utiliser une instance de position en clé de dictionnaire
+    # -tc- ou dans un ensemble
