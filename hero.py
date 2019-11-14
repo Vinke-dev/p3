ae5084a6f566a1418bce62b15d643473060d453d
Thierry Chappuis <tchappui@users.noreply.github.com>
2019-11-14 08:07:42 +0100

    Update hero.py

--- hero.py
+++ hero.py
@@ -1,3 +1,10 @@
+""" -tc- Utilise une docstring pour documenter ton module.
+        
+Note: voir https://sphinxcontrib-napoleon.readthedocs.io/en/latest/example_google.html
+ou la PEP 257
+        
+"""
+
 from position import Position
 from items import Items
 from labyrinth import Labyrinth
@@ -5,7 +12,20 @@ from labyrinth import Labyrinth
 
 
 class Hero:
+    """ -tc- Utilise une docstring pour documenter ton module.
+        
+    Note: voir https://sphinxcontrib-napoleon.readthedocs.io/en/latest/example_google.html
+    ou la PEP 257
+        
+    """
+    
     def __init__(self,labyrinth): 
+        """ -tc- Utilise une docstring pour documenter ta classe.
+
+        Note: voir https://sphinxcontrib-napoleon.readthedocs.io/en/latest/example_google.html
+        ou la PEP 257
+
+        """
         # -tc- Pourquoi instancer Position alors que le héro ne sait pas 
         # -tc- sur quelle position il doit se positionner. Autant recevoir 
         # -tc- cette position en paramètre puis self.position = position
@@ -16,6 +36,12 @@ class Hero:
         self.items = Items()
 
     def move(self, direction):
+        """ -tc- Utilise une docstring pour documenter ta méthode.
+
+        Note: voir https://sphinxcontrib-napoleon.readthedocs.io/en/latest/example_google.html
+        ou la PEP 257
+
+        """
         # nouvelle_position <- demander à l'objet self.position de renvoyer la position dans direction
         new_position = self.position.get_new_position_from_direction(direction)
         # SI nouvelle_position est dans les chemins du labyrinthe:
@@ -27,14 +53,29 @@ class Hero:
            
     # ramasser l'objet
     def catch_item(self):
+        """ -tc- Utilise une docstring pour documenter ta méthode.
+
+        Note: voir https://sphinxcontrib-napoleon.readthedocs.io/en/latest/example_google.html
+        ou la PEP 257
+
+        """
         # item - Demander à self.labyrinthe de me renvoyer l'objet situé à self.position
         item = self.labyrinth.get_item_located_at(self.position)
         # AJouter objet dans la liste self.bag (self.bag.append(item))
         self.bag.append(item)
 
     def fight_guardian(self):
+        """ -tc- Utilise une docstring pour documenter ta méthode.
+
+        Note: voir https://sphinxcontrib-napoleon.readthedocs.io/en/latest/example_google.html
+        ou la PEP 257
+
+        """
         if self.position == self.labyrinth.end:
+            # -tc- Pourquoi 3 et qu'est-ce que ce 3 signifie? Plutot utiliser une constante comme NUMBER_OF_ITEMS
             if self.bag == 3:
+                # -tc- Des constantes comme Game.VICTORY, Game.GAME_OVER et Game.CONTINUE seraient plus parlantes 
+                # -tc- (à définir éventuellement dans une classe Game)
                 return 1
             else:
                 return 0
